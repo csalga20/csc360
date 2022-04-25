219c6cc521df6b41ec6a7c1f2d6fef8c7a1dd5c8
Charlie Salgado <csalga20@depaul.edu>
2022-04-25 16:22:12 -0500

    edited and added logout to app

--- Homework2/todo/src/App.js
+++ Homework2/todo/src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
 import Login from './Login';
+import Logout from './Logout';
 
 function App() {
   return (
     <div>
       <Login />
+      <Logout />
     </div>
   );
 }
