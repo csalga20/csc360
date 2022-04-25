8b48ea02c66595b3435f121338e5eee3d6f47cf0
Charlie Salgado <csalga20@depaul.edu>
2022-04-25 15:28:39 -0500

    login complete

--- Homework2/todo/src/Login.js
+++ Homework2/todo/src/Login.js
@@ -13,11 +13,11 @@ export default function Login ({setUser})
     (
         <form onSubmit = {(e) => {e.preventDefault(); setUser(username)}}>
             <label htmlFor="login-username">Username:</label>
-            <input type="text" name="login-username" value={username} onChange={handleUsername}
-    id="login-username" />
+            <input type="text" name="login-username" value={username} onChange={handleUsername} 
+            id="login-username" />
             <label htmlFor="login-password">Password:</label>
             <input type="password" name="login-password" id="login-password" />
             <input type="submit" value="Login" disabled={username.length === 0} />
-      </form>
+        </form>
     );
 }
